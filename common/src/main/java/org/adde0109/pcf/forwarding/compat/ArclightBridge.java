b83c3b832a0f4fc5343e762a69a89bea3df64058
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-01-26 23:19:38 -0600

    Fixed wording

--- common/src/main/java/org/adde0109/pcf/forwarding/compat/ArclightBridge.java
+++ common/src/main/java/org/adde0109/pcf/forwarding/compat/ArclightBridge.java
@@ -6,7 +6,7 @@ public interface ArclightBridge {
     /**
      * <a
      * href="https://github.com/IzzelAliz/Arclight/blob/Trials/arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/ServerLoginNetHandlerMixin.java">Used
-     * in Arclight 1.14 - 1.20.1</a>
+     * for Arclight 1.14 - 1.20.1</a>
      */
     interface V14 {
         void arclight$preLogin() throws Exception;
@@ -15,7 +15,7 @@ public interface ArclightBridge {
     /**
      * <a
      * href="https://github.com/IzzelAliz/Arclight/blob/Net/arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/ServerLoginNetHandlerMixin.java">Used
-     * in Arclight 1.20.2 - 1.20.3</a>
+     * for Arclight 1.20.2 - 1.20.3</a>
      */
     interface V20_2 {
         void arclight$preLogin(GameProfile profile) throws Exception;
@@ -24,8 +24,8 @@ public interface ArclightBridge {
     /**
      * <a
      * href="https://github.com/IzzelAliz/Arclight/blob/Whisper/arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/ServerLoginNetHandlerMixin.java">Used
-     * in Arclight 1.20.4+.</a> <br>
-     * {@link SpigotLoginHandler.V2#callPlayerPreLoginEvents(GameProfile)} would be used, but it is
+     * for Arclight 1.20.4+.</a> <br>
+     * {@link SpigotLoginHandler.V20_5#callPlayerPreLoginEvents(GameProfile)} could be used, but it is
      * private in <a
      * href="https://github.com/IzzelAliz/Arclight/blob/FeudalKings/arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/ServerLoginPacketListenerImplMixin.java">Arclight
      * 1.20.5+.</a>
