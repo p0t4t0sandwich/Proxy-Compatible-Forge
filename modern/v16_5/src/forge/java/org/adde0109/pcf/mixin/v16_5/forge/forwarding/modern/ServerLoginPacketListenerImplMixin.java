2fc6e6411e2ed131d3dd7a0e0b5428b1b32f5b7b
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-01-29 23:10:42 -0600

    Added Arclight-specific HandleHello mixins due to their overwrites

--- modern/v16_5/src/forge/java/org/adde0109/pcf/mixin/v16_5/forge/forwarding/modern/ServerLoginPacketListenerImplMixin.java
+++ modern/v16_5/src/forge/java/org/adde0109/pcf/mixin/v16_5/forge/forwarding/modern/ServerLoginPacketListenerImplMixin.java
@@ -9,6 +9,7 @@ import dev.neuralnexus.taterapi.meta.Mappings;
 import dev.neuralnexus.taterapi.meta.anno.AConstraint;
 import dev.neuralnexus.taterapi.meta.anno.Versions;
 import dev.neuralnexus.taterapi.meta.enums.MinecraftVersion;
+import dev.neuralnexus.taterapi.meta.enums.Platform;
 
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
@@ -17,6 +18,7 @@ import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 
 import org.adde0109.pcf.forwarding.modern.ConnectionBridge;
 import org.adde0109.pcf.forwarding.modern.ServerLoginPacketListenerBridge;
+import org.apache.commons.lang3.Validate;
 import org.apache.logging.log4j.Logger;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -43,10 +45,19 @@ public abstract class ServerLoginPacketListenerImplMixin
     // spotless:on
 
     // spotless:off
+    @AConstraint(platform = Platform.ARCLIGHT, invert = true)
     @Inject(method = "handleHello", cancellable = true, at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, ordinal = 1,
             target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;state:Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;"))
     // spotless:on
     private void onHandleHello(final @NonNull CallbackInfo ci) {
+        handleHello(this, ci);
+    }
+
+    @AConstraint(platform = Platform.ARCLIGHT)
+    @Inject(method = "handleHello", cancellable = true, at = @At(value = "HEAD"))
+    private void onHandleHelloArclight(final @NonNull CallbackInfo ci) {
+        Validate.validState(
+                this.state == ServerLoginPacketListenerImpl.State.HELLO, "Unexpected hello packet");
         handleHello(this, ci);
     }
 
--- modern/v21_11/src/main/java/org/adde0109/pcf/mixin/v21_11/forwarding/modern/ServerLoginPacketListenerImplHelloMixin.java
+++ modern/v21_11/src/main/java/org/adde0109/pcf/mixin/v21_11/forwarding/modern/ServerLoginPacketListenerImplHelloMixin.java
@@ -4,24 +4,33 @@ import static org.adde0109.pcf.forwarding.modern.ModernForwarding.handleHello;
 
 import dev.neuralnexus.taterapi.meta.Mappings;
 import dev.neuralnexus.taterapi.meta.anno.AConstraint;
+import dev.neuralnexus.taterapi.meta.anno.AConstraints;
 import dev.neuralnexus.taterapi.meta.anno.Versions;
 import dev.neuralnexus.taterapi.meta.enums.MinecraftVersion;
+import dev.neuralnexus.taterapi.meta.enums.Platform;
+
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 
 import org.adde0109.pcf.forwarding.modern.ServerLoginPacketListenerBridge;
+import org.apache.commons.lang3.Validate;
 import org.jspecify.annotations.NonNull;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // spotless:off
-@AConstraint(version = @Versions(min = MinecraftVersion.V17))
+@AConstraints({
+    @AConstraint(platform = Platform.ARCLIGHT, invert = true),
+    @AConstraint(version = @Versions(min = MinecraftVersion.V17))
+})
 @SuppressWarnings({"MixinAnnotationTarget", "UnresolvedMixinReference"})
 @Mixin(targets = "net.minecraft.server.network.ServerLoginPacketListenerImpl")
 public abstract class ServerLoginPacketListenerImplHelloMixin
         implements ServerLoginPacketListenerBridge {
-    @AConstraint(mappings = Mappings.SEARGE, version = @Versions(min = MinecraftVersion.V17, max = MinecraftVersion.V18_2))
+    @AConstraint(mappings = Mappings.SEARGE, version = @Versions(max = MinecraftVersion.V18_2))
     @Inject(method = "m_5990_", cancellable = true, at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, ordinal = 1,
             target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;f_10019_:Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;"))
     private void onHandleHello_17(final @NonNull CallbackInfo ci) {
@@ -47,6 +56,19 @@ public abstract class ServerLoginPacketListenerImplHelloMixin
             target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;startClientVerification(Lcom/mojang/authlib/GameProfile;)V"))
     private void onHandleHello_20_M(final @NonNull CallbackInfo ci) {
         handleHello(this, ci);
+    }
+
+    @AConstraint(platform = Platform.ARCLIGHT, version = @Versions(min = MinecraftVersion.V17))
+    @Mixin(targets = "net.minecraft.server.network.ServerLoginPacketListenerImpl")
+    public static class ArclightMixin {
+        @Shadow private ServerLoginPacketListenerImpl.State state;
+
+        @Inject(method = {"handleHello", "m_5990_"}, require = 0, cancellable = true, at = @At(value = "HEAD"))
+        private void onHandleHelloArclight(final @NonNull CallbackInfo ci) {
+            Validate.validState(
+                    this.state == ServerLoginPacketListenerImpl.State.HELLO, "Unexpected hello packet");
+            handleHello((ServerLoginPacketListenerBridge) this, ci);
+        }
     }
 }
 // spotless:on
