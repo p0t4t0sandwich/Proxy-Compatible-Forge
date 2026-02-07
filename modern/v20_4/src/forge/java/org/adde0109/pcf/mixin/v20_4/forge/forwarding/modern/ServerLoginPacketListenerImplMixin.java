aa32c256c6aafa34dff290e05a833c731c85d14c
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-02-06 19:28:49 -0600

    Added a min version constraint to a 1.20.2+ field, the mixin should behave as expected now for older versions

--- modern/v20_4/src/forge/java/org/adde0109/pcf/mixin/v20_4/forge/forwarding/modern/ServerLoginPacketListenerImplMixin.java
+++ modern/v20_4/src/forge/java/org/adde0109/pcf/mixin/v20_4/forge/forwarding/modern/ServerLoginPacketListenerImplMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import org.adde0109.pcf.forwarding.modern.ConnectionBridge;
 import org.adde0109.pcf.forwarding.modern.ServerLoginPacketListenerBridge;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,8 +31,9 @@ public abstract class ServerLoginPacketListenerImplMixin
     @AConstraint(version = @Versions(min = MinecraftVersion.V20_2))
     @Shadow abstract void shadow$startClientVerification(GameProfile profile);
 
-    @Shadow
-    private GameProfile authenticatedProfile;
+    @AConstraint(version = @Versions(min = MinecraftVersion.V20_2))
+    @Shadow private @Nullable GameProfile authenticatedProfile;
+
     @Unique private int pcf$velocityLoginMessageId = -1;
     // spotless:on
 
