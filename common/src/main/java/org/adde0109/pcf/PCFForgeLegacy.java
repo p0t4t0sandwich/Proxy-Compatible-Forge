2d5fcf6beff2c8205e2015e52d76aa60e8506f18
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-03-14 19:20:49 -0600

    Hopefully fixed old Forge entrypoints

--- common/src/main/java/org/adde0109/pcf/PCFForgeLegacy.java
+++ common/src/main/java/org/adde0109/pcf/PCFForgeLegacy.java
@@ -2,7 +2,7 @@ package org.adde0109.pcf;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = PCF.MOD_ID, acceptableRemoteVersions = "*", useMetadata = true)
+@Mod(modid = PCF.MOD_ID, name = PCF.MOD_NAME, acceptableRemoteVersions = "*", useMetadata = true)
 public final class PCFForgeLegacy {
     public PCFForgeLegacy() {
         PCF.instance().onInit();
