9f2c068d2ee3cfb55acca972a5dc926fa6bc61a9
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-04-14 20:00:40 -0600

    Added version property to Mod annotations to silence warnings

--- common/src/main/java/org/adde0109/pcf/PCFForgeLegacy.java
+++ common/src/main/java/org/adde0109/pcf/PCFForgeLegacy.java
@@ -2,7 +2,12 @@ package org.adde0109.pcf;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = PCF.MOD_ID, name = PCF.MOD_NAME, acceptableRemoteVersions = "*", useMetadata = true)
+@Mod(
+        modid = PCF.MOD_ID,
+        name = PCF.MOD_NAME,
+        version = PCF.VERSION,
+        acceptableRemoteVersions = "*",
+        useMetadata = true)
 public final class PCFForgeLegacy {
     public PCFForgeLegacy() {
         PCF.instance().onInit();
